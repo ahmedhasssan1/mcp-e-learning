a49a683dba1fd328ce65945ed06608c621e8191e
ahmedhasssan1 <ahmedkhel2003@gmail.com>
2025-12-03 21:26:50 +0200

    feat: add ConfigModule for environment variable management and update database connection settings

--- apps/users/src/users.module.ts
+++ apps/users/src/users.module.ts
@@ -2,17 +2,21 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+     ConfigModule.forRoot({
+      isGlobal: true,   // VERY IMPORTANT
+    }),
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: '127.0.0.1',
-      port: 3306,
-      username: 'root',
-      password: 'Ahmed123*#',
-      database: 'e-learning',
-      synchronize: true,
+      type: 'mysql', 
+      host: process.env.DB_HOST,
+      port: 3306, 
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DATABASE,
+      synchronize: true, 
     }),
   ],
   controllers: [UsersController],
