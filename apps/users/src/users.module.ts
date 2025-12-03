3be5b6304b2b26d0215cb2c3b3a5505facdb7c51
ahmedhasssan1 <ahmedkhel2003@gmail.com>
2025-12-03 17:50:29 +0200

    feat: add TypeORM and MySQL2 dependencies; configure VSCode SQLTools for MySQL connection

--- apps/users/src/users.module.ts
+++ apps/users/src/users.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: 'Ahmed123*#',
+      database: 'e-learning',
+      synchronize: true,
+    }),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
 })
