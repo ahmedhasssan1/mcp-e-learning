41377e4e5471beea4f70960eccb362c5c9554ab7
ahmedhasssan1 <ahmedkhel2003@gmail.com>
2025-12-26 19:48:57 +0200

    web sockets message

--- apps/courses-api-gateway/src/websocket/websocket.gateway.ts
+++ apps/courses-api-gateway/src/websocket/websocket.gateway.ts
@@ -1,9 +1,17 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
-
+import { SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import {Server} from "socket.io"
 @WebSocketGateway()
 export class WebsocketGateway {
+
+  @WebSocketServer()
+  server:Server
+
   @SubscribeMessage('message')
   handleMessage(client: any, payload: any): string {
     return 'Hello from websocketgatway';
   }
+  SendMessage(){
+    this.server.emit("NewMessage",'hello from the server')
+  }
+  
 }
