20e67899e4bf21ea957d76d402e458ab6d16f8f5
ahmedhasssan1 <ahmedkhel2003@gmail.com>
2026-01-12 21:53:06 +0200

    send message withoud authorId it gets auto

--- apps/courses-api-gateway/src/websocket/websocket.controller.ts
+++ apps/courses-api-gateway/src/websocket/websocket.controller.ts
@@ -1,13 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Req } from '@nestjs/common';
 import { WebsocketService } from './websocket.service';
 import { createMessageDto } from 'libs/messages/dto/createMessaga.dto';
 
 @Controller('websocket')
 export class WebsocketController {
   constructor(private readonly webSocketService: WebsocketService) {}
-  
+
   @Post('newMessgae')
-  async newMessage(@Body() message: createMessageDto) {
-    return await this.webSocketService.createMessage(message);
+  async newMessage(@Body() message: createMessageDto, @Req() req: any) {
+    console.log('debugging request ', req);
+
+    const token = req.headers.authorization;
+    console.log('get token', token);
+
+    return await this.webSocketService.createMessage(message, token);
   }
 }
