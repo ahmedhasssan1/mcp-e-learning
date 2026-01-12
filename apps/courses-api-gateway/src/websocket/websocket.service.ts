325d3d0e27a899716721c4d6ef066b19be17b4c0
ahmedhasssan1 <ahmedkhel2003@gmail.com>
2026-01-12 21:53:18 +0200

    send message withoud authorId it gets auto

--- apps/courses-api-gateway/src/websocket/websocket.service.ts
+++ apps/courses-api-gateway/src/websocket/websocket.service.ts
@@ -1,34 +1,63 @@
 import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
 import { ClientKafka } from '@nestjs/microservices';
 import { QueueName } from 'apps/courses-api-gateway/enums/queue-name';
-import { createMessageDto } from 'libs/messages/dto/createMessaga.dto';
+import {
+  createMessageDto,
+  savedMwssage,
+} from 'libs/messages/dto/createMessaga.dto';
 import { firstValueFrom, timeout } from 'rxjs';
 import { WebsocketGateway } from './websocket.gateway';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class WebsocketService implements OnModuleInit {
   constructor(
     @Inject(QueueName.KAFKA_SERVICE) private kafkaClient: ClientKafka,
     private readonly wsGateway: WebsocketGateway,
+    private configService: ConfigService,
+    private jwtService: JwtService,
   ) {}
   async onModuleInit() {
     this.kafkaClient.subscribeToResponseOf('create_message');
     await this.kafkaClient.connect();
   }
-  async createMessage(createMessage: createMessageDto) {
+  async createMessage(createMessage: createMessageDto, token: any) {
     try {
-      
-      const NewMessage = await firstValueFrom(
-        this.kafkaClient
-          .send('create_message', createMessage)
-          .pipe(timeout(5000)),
-      );
-      console.log(' Received response from Kafka:', NewMessage);
+      if (!token) {
+        throw new Error('Token is required');
+      }
 
-      await this.wsGateway.sendMessage(createMessage);
+      const verifytoken = await this.jwtService.verifyAsync(token, {
+        secret: this.configService.get<string>('JWT_PASS'),
+      });
+
+      const userId = Number(verifytoken.sub);
+      console.log('Verified token:', verifytoken);
+      console.log('User ID:', userId, 'Type:', typeof userId);
+
+      if (isNaN(userId)) {
+        throw new Error('Invalid user ID from token');
+      }
+
+      const message: savedMwssage = {
+        authorId: userId,
+        content: createMessage.content,
+        conversationId: createMessage.conversationId,
+      };
+
+      console.log('Message to send:', message);
+
+      const NewMessage = await firstValueFrom(
+        this.kafkaClient.send('create_message', message).pipe(timeout(5000)),
+      );
+      console.log('Received response from Kafka:', NewMessage);
+
+      await this.wsGateway.sendMessage(message);
       return NewMessage;
     } catch (err) {
-      console.log('catch error can not handle createmessage function');
+      console.error('Error in createMessage:', err.message);
+      throw err;
     }
   }
 }
