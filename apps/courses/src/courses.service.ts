bcdb0bd959b183ab9fd7c0357d4f360b23fab155
ahmedhasssan1 <ahmedkhel2003@gmail.com>
2026-01-19 20:11:46 +0200

    add get all course method

--- apps/courses/src/courses.service.ts
+++ apps/courses/src/courses.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Course } from './courses/entities/course.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CoursesService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(@InjectRepository(Course) private readonly courseRepo:Repository<Course>){}
+
+  async getAllCourses(){
+    const courses=await this.courseRepo.find();
+    return courses
   }
 }
