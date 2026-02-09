92fb687d324915e09ab5ee6a423f63ead4cec714
ahmedhasssan1 <ahmedkhel2003@gmail.com>
2026-02-09 09:41:56 +0200

    cretae brach on clone (remorte)

--- apps/courses/src/courses.service.ts
+++ apps/courses/src/courses.service.ts
@@ -22,7 +22,7 @@ export class CoursesService {
     if (!course_exist) {
       throw new Error('Course not found');
     }
-    console.log('update in local  repo ');
+    console.log('create branch on remote ');
     return await this.courseRepo.remove(course_exist);
     
   }
