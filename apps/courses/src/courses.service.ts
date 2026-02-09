dbd331d9fd49c9ce4f5ffad87dae072989e5fdce
ahmedhasssan1 <ahmedkhel2003@gmail.com>
2026-02-09 03:57:45 +0200

    test pull req

--- apps/courses/src/courses.service.ts
+++ apps/courses/src/courses.service.ts
@@ -22,7 +22,7 @@ export class CoursesService {
     if (!course_exist) {
       throw new Error('Course not found');
     }
-    console.log('testing edit in test branch');
+    console.log('trying to create pull request');
     return await this.courseRepo.remove(course_exist);
     
   }
